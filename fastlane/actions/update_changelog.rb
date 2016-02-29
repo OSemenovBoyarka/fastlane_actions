823b097f5002d5ae3f984012db7c5f9c13345d29
Alexander Semenov <asemenovboyarka@gmail.com>
2016-02-29 12:11:48 +0200

    fixed code styling

--- fastlane/actions/update_changelog.rb
+++ fastlane/actions/update_changelog.rb
@@ -11,78 +11,75 @@ module Fastlane
     # - Commit, push and submit the pull request
 
     class UpdateChangelogAction < Action
-    	CHANGELOG_TEMPLATE_LEGACY = "### Fixed\n*\n\n### Added\n*\n\n### Removed\n*"
-    	CHANGELOG_TEMPLATE = "* Put your changes here one by one, you can use markdown syntax"
-    
-      def self.run(params)
+      CHANGELOG_TEMPLATE_LEGACY = "### Fixed\n*\n\n### Added\n*\n\n### Removed\n*".freeze
+      CHANGELOG_TEMPLATE = '* Put your changes here one by one, you can use markdown syntax'.freeze
 
-        changelogFilename ='CHANGELOG.md';
-        currentChangelogFilename ='CHANGELOG_CURRENT.md';
+      def self.run(params)
+        changelogFilename = 'CHANGELOG.md'
+        currentChangelogFilename = 'CHANGELOG_CURRENT.md'
 
         version = params[:version]
 
         currentChangeLog = File.read(currentChangelogFilename).to_s.strip
-        changelogErrorMessage = "You have not provided changelog for build. Please, fill in file CHANGELOG_CURRENT.md";
-      	raise changelogErrorMessage unless CHANGELOG_TEMPLATE.to_s != currentChangeLog.to_s
-      	raise changelogErrorMessage unless CHANGELOG_TEMPLATE_LEGACY.to_s != currentChangeLog.to_s
-        globalChangeLog = File.read(changelogFilename);
-#       prepending contents of current changelog to existing file
+        changelogErrorMessage = 'You have not provided changelog for build. Please, fill in file CHANGELOG_CURRENT.md'
+        raise changelogErrorMessage unless CHANGELOG_TEMPLATE.to_s != currentChangeLog.to_s
+        raise changelogErrorMessage unless CHANGELOG_TEMPLATE_LEGACY.to_s != currentChangeLog.to_s
+        globalChangeLog = File.read(changelogFilename)
+        #       prepending contents of current changelog to existing file
         File.open(changelogFilename, 'w') do |fo|
-                currentDate = Time.now.strftime('%Y-%m-%d')
-                fo.puts "## #{version} - #{ currentDate }"
-                fo.puts currentChangeLog
-                fo.puts "\n"
-                fo.puts globalChangeLog
+          currentDate = Time.now.strftime('%Y-%m-%d')
+          fo.puts "## #{version} - #{currentDate}"
+          fo.puts currentChangeLog
+          fo.puts "\n"
+          fo.puts globalChangeLog
         end
-#        deleting current changelog file to make it clean for next releases
+        #        deleting current changelog file to make it clean for next releases
         File.open(currentChangelogFilename, 'w') do |fo|
-        	fo.puts CHANGELOG_TEMPLATE
+          fo.puts CHANGELOG_TEMPLATE
         end
-    
-      Actions.lane_context[SharedValues::UPDATE_CHANGELOG_CURRENT] = currentChangeLog
+
+        Actions.lane_context[SharedValues::UPDATE_CHANGELOG_CURRENT] = currentChangeLog
       end
-
-
 
       #####################################################
       # @!group Documentation
       #####################################################
 
       def self.description
-        "Reads current changelog and appends it to global one"
+        'Reads current changelog and appends it to global one'
       end
 
       def self.details
         # Optional:
         # this is your change to provide a more detailed description of this action
-        "You can use this action to provide change log for olg version and update global one with it. You must have CHANGELOG_CURRENT.md file with changes for current version. You can use standard markdown. It then be moved to CHANGELOG.md as changes for current version and cleaned"
+        'You can use this action to provide change log for olg version and update global one with it. You must have CHANGELOG_CURRENT.md file with changes for current version. You can use standard markdown. It then be moved to CHANGELOG.md as changes for current version and cleaned'
       end
 
       def self.available_options
-[
-FastlaneCore::ConfigItem.new(key: :version,
-                             env_name: "FL_UPDATE_CHANGELOG_VERSION", # The name of the environment variable
-                             description: "Complete version string, which will be added as current version to changelog", # a short description of this parameter
-                             verify_block: proc do |value|
-                             raise "No new app version for UpdateChangeLog given, pass using `version: 'token'`".red unless (value and not value.empty?)
-                             end)
-]
+        [
+          FastlaneCore::ConfigItem.new(key: :version,
+                                       env_name: 'FL_UPDATE_CHANGELOG_VERSION', # The name of the environment variable
+                                       description: 'Complete version string, which will be added as current version to changelog', # a short description of this parameter
+                                       verify_block: proc do |value|
+                                                       raise "No new app version for UpdateChangeLog given, pass using `version: 'token'`".red unless value && !value.empty?
+                                                     end)
+        ]
         # Define all options your action supports.
       end
 
       def self.output
         # Define the shared values you are going to provide
         # Example
-          ['UPDATE_CHANGELOG_CURRENT', 'Current version changelog string representation, you can send it to slack, for instance']
+        ['UPDATE_CHANGELOG_CURRENT', 'Current version changelog string representation, you can send it to slack, for instance']
       end
 
       def self.authors
         # So no one will ever forget your contribution to fastlane :) You are awesome btw!
-        ["SemenovAlexander"]
+        ['SemenovAlexander']
       end
 
       def self.is_supported?(platform)
-         [:ios, :mac, :android].include? platform
+        [:ios, :mac, :android].include? platform
       end
     end
   end
